bbf84e4b0bd3086ab46a21709feb90eeb0734120
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-05-23 23:10:19 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,41 +8,6 @@ function Product() {
         <Book />
         <div class="product_body">
           <div class="product_description">
-            <p>
-              Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with
-              K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl
-            </p>
-          </div>
-          <div class="product_price">
-            <p>
-              &#8377; <span>2000</span>
-            </p>
-          </div>
-          <ul class="rating d-flex">
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="far fa-star"></i>
-            </li>
-          </ul>
-          <div class="product_img d-flex">
-            <img src="assets/product2.jpg" alt="" />
-          </div>
-          <div class="button-cart d-flex">
-            <a href="#">
-              <button>Add To Busket</button>
-            </a>
-          </div>
-        </div>
-        <div class="product_body">
-          <div class="product_description">
             <p>Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor</p>
           </div>
           <div class="product_price">
