4dafe62ef1550d64b7803f42c674125f6dbe6a62
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-05-24 11:44:52 +0530

    import Footer to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Banner from "./Banner";
 import Product from "./Product";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Banner />
       <Product />
+      <Footer />
     </div>
   );
 }
