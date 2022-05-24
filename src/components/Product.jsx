a307badf5d5907cc047f9a65d52b3c2b160e26bb
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-05-24 09:46:31 +0530

    import Tracker

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Book from "./Book";
 import Mixer from "./Mixer";
+import Tracker from "./Tracker";
 
 function Product() {
   return (
@@ -8,38 +9,7 @@ function Product() {
       <div class="product_row d-flex">
         <Book />
         <Mixer />
-        <div class="product_body">
-          <div class="product_description">
-            <p>Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor</p>
-          </div>
-          <div class="product_price">
-            <p>
-              &#8377; <span>2500</span>
-            </p>
-          </div>
-          <ul class="rating d-flex">
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="fas fa-star"></i>
-            </li>
-            <li>
-              <i class="far fa-star"></i>
-            </li>
-          </ul>
-          <div class="product_img d-flex">
-            <img src="assets/product3.jpg" alt="" />
-          </div>
-          <div class="button-cart d-flex">
-            <a href="#">
-              <button>Add To Busket</button>
-            </a>
-          </div>
-        </div>
+        <Tracker />
       </div>
 
       <div class="product_row d-flex">
